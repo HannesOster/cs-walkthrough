4a40149a51b0dbff09cb902f254c58251ad748fd
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-11-10 17:54:00 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace HalloWelt
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool mobile = true;
+            bool qualified = true;
+            bool examPassed = false;
+            bool shy = false;
+
+            if (mobile && (qualified || examPassed) && !shy)
+            {
+                Console.WriteLine("Angenommen");
+            }
+            else
+            {
+                Console.WriteLine("Abgelehnt");
+            }
+        }
+
+    }
+}
